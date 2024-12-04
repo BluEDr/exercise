0d6a1ecc155062fa3f1f1b525706e1cbc52904a5
bluedr <skordos88@gmail.com>
2024-12-05 00:24:47 +0200

    wrong grater sign

--- js/validations.js
+++ js/validations.js
@@ -1,7 +1,7 @@
 function isGreaterThanTen() {
     const num = document.getElementById("input");
     const inputValue = parseInt(num.value); 
-    if(inputValue>=10)
+    if(inputValue>10)
         alert("Value accepted.");
     else
         alert("Please try again with a number greater than 10.");
